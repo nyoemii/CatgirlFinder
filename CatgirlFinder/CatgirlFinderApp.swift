3efb59ea2f60b83f1fb5983665cd5d893555eb36
Noemi <hdxdlolmc@gmail.com>
2025-04-12 11:33:54 +0200

    Initial Commit

---
+++ CatgirlFinder/CatgirlFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatgirlFinderApp.swift
+//  CatgirlFinder
+//
+//  Created by Noemi Vrhovcak on 12.04.25.
+//
+
+import SwiftUI
+
+@main
+struct CatgirlFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
